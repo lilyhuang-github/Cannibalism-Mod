f3d827cff1fa45e6f6df2fc0735cf3b789907aa1
lily <lilyhuang-github@users.noreply.github.com>
2024-07-27 21:24:07 -0400

    init

---
+++ src/main/java/net/lily/cannibalismmod/CannibalismModClient.java
@@ -0,0 +1,10 @@
+package net.lily.cannibalismmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CannibalismModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/lily/cannibalismmod/CannibalismModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lily.cannibalismmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CannibalismModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
